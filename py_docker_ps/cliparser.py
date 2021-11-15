5bb6cbf845ea5b982f643262ff6e5e2c9c3133fa
Stevan Vanderwerf <github@vanderling.com>
2021-11-15 06:42:30 -0600

    added arguments for argparse module

--- py_docker_ps/cliparser.py
+++ py_docker_ps/cliparser.py
@@ -9,24 +9,13 @@ def parse_cli_args():
         description='use the docker ps command in python',
         add_help=True
     )
-    my_parser.version = f"py_docker_ps version: {__version__}"
 
     my_parser.add_argument(
         '-a',
         '--all',
-        action='store',
+        action='store_true',
+        default=False,
         help='Show all containers (default shows just running)'
-    )
-    my_parser.add_argument(
-        '-f',
-        '--filter',
-        action='store',
-        help='Filter output based on conditions provided'
-    )
-    my_parser.add_argument(
-        '--format',
-        action='store',
-        help='Pretty-print containers using a Go template'
     )
     my_parser.add_argument(
         '-n',
@@ -38,25 +27,22 @@ def parse_cli_args():
         '-l',
         '--latest',
         action='store',
+        default=0,
+        type=int,
         help='Show the latest created container (includes all states)'
     )
     my_parser.add_argument(
-        '--no-trunc',
-        action='store',
+        '--notrunc',
+        action='store_true',
+        default=False,
         help='Don\'t truncate output'
     )
     my_parser.add_argument(
         '-q',
         '--quiet',
-        action='store',
+        action='store_true',
+        default=False,
         help='Only display container IDs'
-    )
-    my_parser.add_argument(
-        '-s',
-        '--size',
-        action='store',
-        nargs='store',
-        help='Display total file sizes'
     )
 
     args = vars(my_parser.parse_args())
